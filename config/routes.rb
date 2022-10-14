6b121f7bbb3c18fc25b9ca35c209e760e1bb15a7
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-10-13 23:02:15 -0500

    Added basic oauth, with routing, need to fix multiple minor bugs.

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/')
 end
