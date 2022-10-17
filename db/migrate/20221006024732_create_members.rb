54eab8bb6db1da9b1ca41f6945d84eb4c3abc829
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-10-17 13:20:03 -0500

    Added minor fixes.

--- db/migrate/20221006024732_create_members.rb
+++ db/migrate/20221006024732_create_members.rb
@@ -3,16 +3,10 @@ class CreateMembers < ActiveRecord::Migration[6.1]
     create_table :members do |t|
       t.string :name
       t.string :email
-<<<<<<< HEAD:db/migrate/20221017061425_create_members.rb
       t.string :token
       t.integer :access_type
       t.integer :paid_status
       t.integer :points
-=======
-      t.integer :points
-      t.string :paid_status
-      t.integer :access_type
->>>>>>> test:db/migrate/20221006024732_create_members.rb
 
       t.timestamps
     end
