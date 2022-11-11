11949a2302068fc846ca94021d52140eafcd9d0d
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-11-11 15:41:32 -0600

    Added time restriction to signing in.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,15 @@ class Event < ApplicationRecord
      has_one :attendance
 
      def self.sign_in_page(current_member, current_event)
-          if current_member 
+          # Compare date time, give a 60 minute period in which members can sign in
+          # 1800 for 30 minutes to sign in before or after the event starts, 21600 for 6 hour offset that comes with utc time zone
+          if current_member && DateTime.now.utc > current_event.date + 19800 && DateTime.now.utc < current_event.date + 23400 
                Attendance.create_attendance(current_member, current_event)
           else
-               redirect_to('/auth/google_oauth2') # Alert they cannot sign in to the event without being signed in to their account
+               puts(DateTime.now.utc)
+               puts(current_event.date + 19800)
+               puts(current_event.date + 23400)
+               redirect_to('../../auth/google_oauth2') # Alert they cannot sign in to the event without being signed in to their account
           end
      end
 
