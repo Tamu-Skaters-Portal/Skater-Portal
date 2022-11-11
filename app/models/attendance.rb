a60d2528ae03af98701f92ec3b0eb30b75562302
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-11-11 15:14:52 -0600

    Restricted to one attendance per event per member.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,7 +5,9 @@ class Attendance < ApplicationRecord
      belongs_to :event
 
      def self.create_attendance(current_member, current_event)
-          @attendance = Attendance.new({member: current_member, event: current_event})
-          @attendance.save
+          if (Attendance.where(member: current_member, event: current_event).last == nil)
+               @attendance = Attendance.new({member: current_member, event: current_event})
+               @attendance.save
+          end
      end
 end
